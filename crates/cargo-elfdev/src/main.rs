d51e570846a1180f2ccb67b72edcbc401269eaaa
Michael Helvey <michael.helvey1@gmail.com>
2023-11-05 14:49:48 -0600

    feat(dev-server): completed simple working dev server

--- crates/cargo-elfdev/src/main.rs
+++ crates/cargo-elfdev/src/main.rs
@@ -1,10 +1,25 @@
 use std::{
+    net::SocketAddr,
     path::Path,
     sync::{Arc, RwLock},
     time::Duration,
+    vec,
 };
+use tokio_util::sync::CancellationToken;
+use tower_http::trace::TraceLayer;
 
+use axum::{
+    extract::{
+        ws::{Message, WebSocketUpgrade},
+        ConnectInfo, State,
+    },
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use axum_macros::debug_handler;
 use color_eyre::Result;
+use futures::{SinkExt, StreamExt};
 use notify::{
     event::{CreateKind, ModifyKind, RemoveKind},
     Event, EventKind, RecommendedWatcher, Watcher,
@@ -15,9 +30,11 @@ use tracing::{debug, info};
 /// The dev server is implemented in a client-server model between Tokio tasks, where many producer
 /// threads wait on various events (signals, files changed, etc), and signal the "manager" thread to
 /// do something in response, such as restarting the dev server, or simply terminating the process.
+#[derive(Clone, Debug)]
 enum DevServerMessage {
     Shutdown { signal: &'static str },
     Restart,
+    ReloadBrowser,
 }
 
 /// The state that we are in makes a difference to how we handle certain kinds of signals.
@@ -119,7 +136,10 @@ async fn create_signal_listener(
 async fn create_manager_process(
     state: Arc<RwLock<DevServerState>>,
     mut rx: tokio::sync::mpsc::Receiver<DevServerMessage>,
+    self_tx: tokio::sync::mpsc::Sender<DevServerMessage>,
+    ws_tx: async_channel::Sender<()>,
     mut cmd: Child,
+    ws_cancel_token: CancellationToken,
 ) -> Result<()> {
     while let Some(msg) = rx.recv().await {
         match msg {
@@ -129,6 +149,7 @@ async fn create_manager_process(
                     signal
                 );
                 cmd.kill().await?;
+                ws_cancel_token.cancel();
                 break;
             }
             DevServerMessage::Restart => {
@@ -149,6 +170,11 @@ async fn create_manager_process(
                     let mut state = state.write().unwrap();
                     *state = DevServerState::Idle;
                 }
+                self_tx.send(DevServerMessage::ReloadBrowser).await?;
+            }
+            DevServerMessage::ReloadBrowser => {
+                debug!("reloading browser...");
+                ws_tx.send(()).await?;
             }
         }
     }
@@ -158,17 +184,18 @@ async fn create_manager_process(
 
 fn create_file_watcher(
     tx: tokio::sync::mpsc::Sender<DevServerMessage>,
+    message: DevServerMessage,
 ) -> Result<RecommendedWatcher> {
     let watcher = RecommendedWatcher::new(
         move |res: notify::Result<Event>| {
-            let restart_server = || {
+            let send_change_signal = || {
                 let rt = tokio::runtime::Builder::new_current_thread()
                     .enable_all()
                     .build()
                     .expect("could not create Tokio runtime to send message on");
 
                 rt.block_on(async {
-                    tx.send(DevServerMessage::Restart)
+                    tx.send(message.clone())
                         .await
                         .expect("could not send file change message to manager process");
                 });
@@ -180,7 +207,7 @@ fn create_file_watcher(
                     | EventKind::Remove(RemoveKind::File)
                     | EventKind::Create(CreateKind::File) => {
                         info!("file changed: {:?}", event.paths);
-                        restart_server();
+                        send_change_signal();
                     }
                     _ => {}
                 },
@@ -195,6 +222,57 @@ fn create_file_watcher(
     Ok(watcher)
 }
 
+#[debug_handler]
+async fn websocket_handler(
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    State(rx): State<Arc<async_channel::Receiver<()>>>,
+) -> impl IntoResponse {
+    debug!("received websocket connection from {addr}");
+    ws.on_upgrade(|mut socket| async move {
+        if socket
+            .send(Message::Ping((vec![1, 2, 3]).into()))
+            .await
+            .is_ok()
+        {
+            debug!("websocket connection established");
+        } else {
+            debug!("websocket connection failed");
+            return;
+        }
+
+        let (mut sender, mut receiver) = socket.split();
+
+        let mut recv_task = tokio::spawn(async move {
+            // When this breaks, that means the the client has hung up the connection.
+            while let Some(Ok(msg)) = receiver.next().await {
+                debug!("received message from browser: {:?}", msg);
+            }
+        });
+
+        let mut send_task = tokio::spawn(async move {
+            // When this breaks, it means that the server has closed the reload channel.
+            while let Ok(_) = rx.recv().await {
+                debug!("received reload signal from manager process");
+                sender.send(Message::Text("reload".into())).await.unwrap();
+            }
+        });
+
+        // When either tasks exits (either because the client closed the connection or because the
+        // server is exiting), abort the other one.
+        tokio::select! {
+            _ = (&mut recv_task) => {
+                debug!("websocket connection closed");
+                send_task.abort();
+            },
+            _ = (&mut send_task) => {
+                debug!("reload channel closed");
+                recv_task.abort();
+            },
+        }
+    })
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
@@ -203,18 +281,66 @@ async fn main() -> Result<()> {
     let state = Arc::new(RwLock::new(DevServerState::Idle));
     let (tx, rx) = tokio::sync::mpsc::channel::<DevServerMessage>(1);
 
+    // Signal listener that sends message to our manager process:
     let signal_listener = tokio::spawn(create_signal_listener(state.clone(), tx.clone()));
 
-    let cmd = start_child_server().await?;
-    let manager_proc = tokio::spawn(create_manager_process(state, rx, cmd));
-
-    // Set up file watcher: note that it's really important that we call watcher.watch() here in the
-    // main function or otherwise the magic of Drop will make the whole thing a noop:
-    let mut watcher = create_file_watcher(tx.clone())?;
+    // Watcher for the Rust server:
+    let mut server_watcher = create_file_watcher(tx.clone(), DevServerMessage::Restart)?;
     let path = Path::new("./crates/elf");
-    info!("watching {:?}", path.canonicalize().unwrap());
-    watcher.watch(path, notify::RecursiveMode::Recursive)?;
+    info!("watching rust server at {:?}", path.canonicalize().unwrap());
+    server_watcher.watch(path, notify::RecursiveMode::Recursive)?;
 
+    // Watcher for the static files:
+    let mut static_watcher = create_file_watcher(tx.clone(), DevServerMessage::ReloadBrowser)?;
+    let path = Path::new("./static");
+    info!(
+        "watching static files at {:?}",
+        path.canonicalize().unwrap()
+    );
+    static_watcher.watch(path, notify::RecursiveMode::Recursive)?;
+
+    // Websocket server for triggering browser reloads:
+    let (ws_tx, ws_rx) = async_channel::unbounded();
+    let ws_rx_ptr = Arc::new(ws_rx);
+
+    let live_reload_server = Router::new()
+        .route(
+            "/",
+            get(|| async { "Elf dev server: connect to websocket at /ws for reload events" }),
+        )
+        .route("/ws", get(websocket_handler))
+        .with_state(ws_rx_ptr)
+        .layer(TraceLayer::new_for_http());
+
+    let addr = "127.0.0.1:6969";
+    let ws_cancel_token = CancellationToken::new();
+    let ws_server_cancel_token = ws_cancel_token.clone();
+
+    debug!("websocket server listening on {addr}");
+    let server = axum::Server::bind(&addr.parse().unwrap())
+        .serve(live_reload_server.into_make_service_with_connect_info::<SocketAddr>())
+        .with_graceful_shutdown(async move {
+            ws_server_cancel_token.cancelled().await;
+            debug!("websocket server shutting down");
+        });
+
+    // Child server process:
+    let cmd = start_child_server().await?;
+
+    // Manager process:
+    let ws_manager_cancel_token = ws_cancel_token.clone();
+    let ws_manager_tx = ws_tx.clone();
+    let manager_self_tx = tx.clone();
+    let manager_proc = tokio::spawn(create_manager_process(
+        state,
+        rx,
+        manager_self_tx,
+        ws_manager_tx,
+        cmd,
+        ws_manager_cancel_token,
+    ));
+
+    server.await?;
     manager_proc.await??;
     signal_listener.await??;
 
